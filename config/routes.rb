5f7fc3fbb40ecbcda390d158a4f55ea5378eefb3
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-11 13:48:58 +0200

    added likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tweets , except: [:edit, :update, :show]
+  resources :tweets , except: [:edit, :update, :show] do
+    resources :likes, only: [:new, :create, :destroy]
+  end
 
   root "tweets#index"
 end
