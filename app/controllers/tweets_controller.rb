12899876fcc582537f0834c7bc0478a15cca869f
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-11 10:32:30 +0200

    added index, show methods

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,14 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  def index 
+    @tweets = Tweet.all
+  end
+  
+  def show 
+    @tweet = Tweet.find(params[:id])
+  end
+
   def new 
     @tweet = current_user.tweets.new
   end
