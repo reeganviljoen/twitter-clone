0e7e5562b71d1a4de74aba3f3eec3dff3e6531d1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-13 17:33:30 +0200

    added show method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,10 @@ class TweetsController < ApplicationController
   def index 
     @tweets = Tweet.all
   end
+  
+  def show 
+    @tweet = Tweet.find(params[:id])
+  end
 
   def new 
     @tweet = current_user.tweets.new
