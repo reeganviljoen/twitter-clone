24bb2ce05f99bb21aa68a580222b175fdbad6862
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-13 11:08:33 +0200

    added dependent destroy

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,8 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
 
-  has_many :comments, class_name: 'Tweet', foreign_key: 'tweet_id'
-
+  has_many :comments, class_name: 'Tweet', foreign_key: 'tweet_id', dependent: :destroy
   belongs_to :tweet, class_name: 'Tweet', optional: true
 
   has_rich_text :content
