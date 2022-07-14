fbc067232cf8170d6fb531061a0fbaa67c6f0370
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-14 15:10:21 +0200

    refactored controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,15 @@
 class LikesController < ApplicationController
   def new
     @tweet = Tweet.find(params[:tweet_id])
-    if @tweet.likes.find_by(tweet_id: @tweet.id).present?
-      @like = @tweet.likes.find_by(tweet_id: @tweet.id)
-    else
-      @like = current_user.likes.new(tweet_id: params[:tweet_id])
-    end
+    
+    @like = current_user.likes.find_or_initialize_by(tweet_id: @tweet.id)
   end
 
   def create
     begin
       @like = current_user.likes.create!(like_params)
-      if params[:like][:comment] == 'true'
-        redirect_to new_tweet_like_path(@like.tweet, comment: true), status: :see_other
-      else
-        redirect_to new_tweet_like_path(@like.tweet), status: :see_other
-      end
+      
+      redirect_to new_tweet_like_path(@like.tweet, comment: params[:like][:comment] == 'true'), status: :see_other
     rescue
       render :new, status: :unprocessable_entity
     end
