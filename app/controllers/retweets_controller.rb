df6ed21e906e252bba8cdb7dad89f690e37502a0
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-15 16:22:41 +0200

    added new create methods

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -1,2 +1,26 @@
 class RetweetsController < ApplicationController
+  def new
+    @tweet = Tweet.find(params[:tweet_id])
+    @retweet = @tweet.retweets.new()
+  end
+
+  def create 
+    binding.pry
+    tweet = Tweet.find(params[:tweet_id])
+    @retweet = tweet.retweets.new(retweet_params)
+    if @retweet.save
+      redirect_to root_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def retweet_params
+    permitted_params = params.permit(:content)
+    
+    permitted_params.merge!(
+      user_id: current_user.id
+    )
+  end
 end
