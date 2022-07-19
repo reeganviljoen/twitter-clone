ee6fb8eeb6757e9ffa8d828100087f8953113e3a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-19 08:43:18 +0200

    added turbo stream broadcast callbacks

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,18 +1,25 @@
-
-
 class Tweet < ApplicationRecord
-
+  include ActionView::RecordIdentifier
+  
   belongs_to :user
   has_many :likes, dependent: :destroy
 
   
-  has_many :comments,  -> { where(tweet_type: 'comment')}, dependent: :destroy, class_name: 'Tweet', 
-  foreign_key: 'tweet_id', inverse_of: 'tweet'
+  has_many :comments,  -> { where(tweet_type: 'comment')}, 
+  dependent: :destroy, class_name: 'Tweet', foreign_key: 'tweet_id', inverse_of: 'tweet'
 
   has_many :retweets,-> { where(tweet_type: 'retweet')},
    dependent: :destroy, class_name: 'Tweet', foreign_key: 'tweet_id', inverse_of: 'tweet'
 
   belongs_to :tweet, class_name: 'Tweet', optional:  true
+
+  after_create_commit lambda {
+    broadcast_prepend_to 'tweets', target:'tweets'
+  }
+  
+  after_destroy_commit lambda {
+    broadcast_remove_to 'tweets', target: "#{dom_id self}"
+  } 
 
   scope :descending_tweets, -> { order(created_at: :desc) }
 
