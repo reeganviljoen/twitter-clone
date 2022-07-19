743e7f1d3d6c727ae33b19c9eeee934602075b5c
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-19 11:15:26 +0200

    refactored model stimulus controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,8 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   close() {
-    const modal = document.getElementsByClassName("modal");
-    modal.remove
-    Turbo.visit('/')
+    const modal_inner = this.element.parentElement;
+    modal_inner.parentElement.remove();
   }
 }
