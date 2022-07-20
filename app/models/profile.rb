0ccc4e0b9ba3d41e5fe4f0a2ead8c6b4b25193a2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-20 11:08:24 +0200

    added profil model

---
+++ app/models/profile.rb
@@ -0,0 +1,9 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :phone,      presence: true
+  validates :description, length: { maximum: 300 }
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_082709) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_090551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_082709) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "tweet_id"
@@ -79,7 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_082709) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -88,6 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_082709) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
+  add_foreign_key "profiles", "users"
   add_foreign_key "tweets", "tweets"
   add_foreign_key "tweets", "users"
 end
