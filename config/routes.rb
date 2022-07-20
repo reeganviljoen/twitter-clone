05f8bacea4e852dfdb14e790499750a8a75bda7e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-20 15:03:47 +0200

    added users show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  
+  resources :users, only: :show
   resources :tweets , except: [:edit, :update, :show] do
     resources :likes, only: [:new, :create, :destroy]
     resources :comments, except: [:edit, :update, :show]
     resources :retweets, only: [:new, :create]
   end
-
-  resources :users, only: :show
-
+  
   root "tweets#index"
 end
