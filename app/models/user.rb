b5dc57cdb55981820698650d36ac0065a850a106
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-20 10:39:59 +0200

    added name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   has_many :tweets 
   has_many :likes
+
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  before_create :set_name
+
+  private
+  def set_name
+    split_email = email.split('@')
+    self.name = split_email[0]
+  end
 end
