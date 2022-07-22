75fce22947b435c2e90cf6e5a5f42781d9a40882
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-22 16:11:28 +0200

    fixed follower followed associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   has_many :tweets , dependent: :destroy
   has_many :likes, dependent: :destroy
   
-  has_many :followeds_followers
+  has_many :follower_join, foreign_key: 'follower_id', class_name: 'FollowedsFollower'
+  has_many :followers, through: :follower_join
 
-  has_many :followers, through: :followeds_followers
-  has_many :followeds, through: :followeds_followers
+  has_many :followed_join, foreign_key: 'followed_id', class_name: 'FollowedsFollower'
+  has_many :followers, through: :followed_join
 
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
