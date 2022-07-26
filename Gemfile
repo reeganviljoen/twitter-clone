2699fb312110a23a1ef4ffaa3513463e33d8ddbb
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-26 14:24:30 +0200

    fixed rspec config

--- Gemfile
+++ Gemfile
@@ -52,7 +52,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'pry-rails'
-  gem 'rspec-rails', '~> 3.5'
+
 end
 
 group :development do
@@ -69,10 +69,10 @@ end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem "capybara"
-  gem "selenium-webdriver"
-  gem "webdrivers"
-
+  # gem "capybara"
+  # gem "selenium-webdriver"
+  # gem "webdrivers"
+  gem 'rspec-rails'
   gem 'factory_bot_rails', '~> 4.0'
   gem 'shoulda-matchers', '~> 3.1'
   gem 'faker'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,21 +38,27 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
-  
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
