1679e8a6e8a5531155c15383b28607d591546f75
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-03 14:12:12 +0200

    refactored following and unfollowing

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,11 +1,12 @@
 class FollowsController < ApplicationController
   def create
-    current_user.follow(params[:user_id])
+    current_user.followees.create!(followee_id: params[:user_id])
     redirect_to user_path(params[:user_id])
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
+    followee = current_user.followees.find_by(followee_id: params[:user_id])
+    followee.destroy
     redirect_to user_path(params[:user_id]), status: :see_other
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,6 @@ class User < ApplicationRecord
   has_many :followers, foreign_key: 'followee_id', class_name: 'Follow'
   has_many :followees, foreign_key: 'follower_id', class_name: 'Follow'
 
-  # has_one :profile, dependent: :destroy
-  # accepts_nested_attributes_for :profile
-
   has_many :taggings
   has_many :tags, through: :taggings, dependent: :destroy
   accepts_nested_attributes_for :tags, allow_destroy: true
@@ -36,15 +33,6 @@ class User < ApplicationRecord
         self.tags << tag
       end
     end	  
-  end
-
-  def follow(followee_id)
-    followees.create!(followee_id: followee_id)
-  end
-
-  def unfollow(followee_id)
-    followee = followees.find_by(followee_id: followee_id)
-    followee.destroy
   end
 
   def following(followee_id)
