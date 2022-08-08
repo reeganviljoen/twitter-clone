479649e3e0276e4f5723bab1ab7b49a32bb5231c
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-08 13:27:19 +0200

    merged profile fields into user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,16 @@ RSpec.describe User, type: :model do
   it { should have_many(:likes) }
   it { should have_many(:followers) }
   it { should have_many(:followees) }
-  it { should have_one(:profile) }
-  it { should accept_nested_attributes_for(:profile) }
   it { should have_many(:taggings) }
   it { should have_many(:tags) }
   it { should accept_nested_attributes_for(:tags) }
+  it { should have_one(:avatar_attachment) }
+  it { should have_one(:background_image_attachment)}
+  
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:phone) }
+  it { should validate_length_of(:description) }
+  
+  
 end
