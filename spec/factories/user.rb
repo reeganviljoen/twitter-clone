291b27dd46aa3b7bf155ae8e9e1be36a0a08bac3
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-08 13:26:11 +0200

    added more fields to users factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+    handle { Faker::Name.unique.name}
+    description { 'Description' }
+    first_name { Faker::Name.name }
+    last_name { Faker::Name.name }
+    phone { Faker::PhoneNumber.phone_number }
+    confirmed_at { DateTime.now }
   end
 end
