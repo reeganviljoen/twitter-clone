f384d8fa0b3f9d155ac6a588c18391fa047822d8
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-23 11:33:28 +0200

    configured ationmailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'reeganviljoen@gmail.com',
+    password:             'pcayfpmozqcnupex',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 end
