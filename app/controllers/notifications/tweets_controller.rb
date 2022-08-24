55577e4b85dcb62a4c24fad5a981aaeaf09527dc
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-24 09:55:16 +0200

    fixed notifications

--- app/controllers/notifications/tweets_controller.rb
+++ app/controllers/notifications/tweets_controller.rb
@@ -2,6 +2,6 @@ class Notifications::TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index 
-    @tweets = Tweet.joins(:mentions).where(mentions: {user_name: current_user.handle}).or(Tweet.where(tweet.user_id: current_user.id)).created_at_desc
+    @tweets = Tweet.left_joins(:mentions).where(mentions: {user_name: current_user.handle}).or(Tweet.where(user_id: current_user.id)).created_at_desc
   end
 end
