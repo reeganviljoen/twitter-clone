2417170280d20841fbeec4d5de01e59efb463fb7
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-25 17:05:41 +0200

    wrote tests to check instance variables of user_path

--- Gemfile
+++ Gemfile
@@ -79,6 +79,7 @@ group :test do
   gem 'shoulda-matchers', '~> 3.1'
   gem 'faker'
   gem 'database_cleaner'
+  gem 'rails-controller-testing'
 end
 
 gem "devise", "~> 4.8"
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,13 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   let(:user_attributes)  { attributes_for(:user) }
   let(:user) { User.create(user_attributes)}
-
+  # let(:tweets) { build_list(:tweet, 5, user_id: user.id)}
   before { sign_in user }
 
   describe 'GET /users/:id' do
-    it 'returns a response' do
-      get user_path(user)
-      expect(response.body).to_not be_empty
+    before { get user_path(user) }
+    it 'returns a succesfull response' do
+      expect(response).to be_successful
+    end
+
+    it 'assigns @user' do
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it 'assigns @tweets' do
+      expect(assigns(:tweets)).to be_a(ActiveRecord::AssociationRelation)
     end
   end
 
