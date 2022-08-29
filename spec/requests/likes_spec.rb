0688371eb17820f5849811427ce8686a03f28168
ReeganCodemaster <reeganviljoen@outlook.com>
2022-08-29 17:32:32 +0200

    added test for post tweets/:tweet_id/like

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Likes', type: :request do
-  let(:user_attributes)  { attributes_for(:user) }
-  let(:user) { User.create(user_attributes)}
+  let!(:user) { create(:user)}
+  let!(:tweet) { create(:tweet, user_id: user.id) }
 
-  let(:tweet_attributes) { attributes_for(:tweet)}
-  let(:tweet) { user.tweets.create(tweet_attributes) }
   before { sign_in user }
 
   describe 'GET tweets/:tweet_id/like/new' do
@@ -17,6 +15,18 @@ RSpec.describe 'Likes', type: :request do
 
     it 'asigns @tweet' do
       expect(assigns(:tweet)).to be_a(Tweet)
+    end
+
+    it 'asigns @like' do
+      expect(assigns(:like)).to be_a(Like)
+    end
+  end
+
+  describe 'GET tweets/:tweet_id/like' do
+    before { post tweet_likes_path(tweet), params: {tweet_id: tweet.id} }
+    
+    it 'redirects to tweets/:tweet_id/like/new' do
+      expect(response).to redirect_to(new_tweet_like_path(tweet))
     end
 
     it 'asigns @like' do
